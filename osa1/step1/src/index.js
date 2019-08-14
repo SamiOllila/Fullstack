75ff4cbfd8950fb54b4e12a9b994de6f609e4971
Sami Ollila <ollila.sami93@gmail.com>
2019-08-14 19:57:27 +0300

    changed 12 to 10

--- osa1/step1/src/index.js
+++ osa1/step1/src/index.js
@@ -43,7 +43,7 @@ const App = () => {
   const course = 'Half Stack application development'
   const part1 = {
     name: 'Fundamentals of React',
-    exercises: 12
+    exercises: 10
   }
   const part2 = {
     name: 'Using props to pass data',
