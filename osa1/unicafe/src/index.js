39d0e0959311254ab4fc6a73a0efd96dea2e79b5
Sami Ollila <ollila.sami93@gmail.com>
2019-08-21 21:40:44 +0300

    Completed 1.7

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -12,6 +12,10 @@ const App = () => {
   const handleNeutral = () => setNeutral(neutral + 1)
   const handleBad = () => setBad(bad + 1)
 
+  const all = good+neutral+bad
+  const average = (good-bad)/(all || 1)
+  const positive = 100*(good/(all || 1))
+
   return (
     <div>
       <h1> give feedback </h1>
@@ -22,6 +26,9 @@ const App = () => {
       <p>good {good}</p> 
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
