e8324e57a412e353970712cf325e18ac05c8a67f
Sami Ollila <ollila.sami93@gmail.com>
2019-08-31 15:50:26 +0300

    Completed 1.8

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -2,6 +2,24 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 
+const Statistics = ({good,neutral,bad}) => {
+  const all = good+neutral+bad
+  const average = (good-bad)/(all || 1)
+  const positive = 100*(good/(all || 1))
+
+  return (
+    <div>
+      <h1> statistics </h1>
+      <p>good {good}</p> 
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -12,23 +30,13 @@ const App = () => {
   const handleNeutral = () => setNeutral(neutral + 1)
   const handleBad = () => setBad(bad + 1)
 
-  const all = good+neutral+bad
-  const average = (good-bad)/(all || 1)
-  const positive = 100*(good/(all || 1))
-
   return (
     <div>
       <h1> give feedback </h1>
       <button onClick={handleGood}> good </button>
       <button onClick={handleNeutral}> neutral </button>
       <button onClick={handleBad}> bad </button>
-      <h1> statistics </h1>
-      <p>good {good}</p> 
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
