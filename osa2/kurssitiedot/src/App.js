c87b7e952666d2998e9830cb5aa16c47c7cb3e4a
Sami Ollila <ollila.sami93@gmail.com>
2019-09-11 19:26:51 +0300

    Started part 2.

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.header}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  const parts = props.parts
+  return (
+    <div>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const parts = props.parts
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  return (
+    <div>
+      <p>Number of exercises {total}</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header header={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,23 +1,49 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
+import Note from './components/Note'
 
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
 
-const App = (props) => {
-  const [value, setValue] = useState(10)
+const App = ({ notes }) => {
 
+  const rows = () => notes.map(note =>
+    <Note
+      key={note.id}
+      note={note}
+    />
+  )
+  
   return (
     <div>
-      {value}
-      <button onClick={() => console.log('clicked the button')}>
-        button
-      </button>
-      <button>reset to zero</button>
+      <h1>Notes</h1>
+      <ul>
+        {rows()}      
+      </ul>
     </div>
   )
 }
 
-
 ReactDOM.render(
-  <App />,
+  <App notes={notes} />,
   document.getElementById('root')
 )
+
