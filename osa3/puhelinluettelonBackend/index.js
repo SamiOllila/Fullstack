14080be4ed2be4df3ce430e296a22eac8d03869b
Sami Ollila <ollila.sami93@gmail.com>
2020-01-27 15:16:01 +0200

    Completed 3.3

--- osa3/puhelinluettelonBackend/index.js
+++ osa3/puhelinluettelonBackend/index.js
@@ -24,6 +24,17 @@ let persons = [
   }
 ]
 
+app.get(`/api/persons/:id`, (req,res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if(person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
 app.get('/info', (req, res) => {
   res.send(`<div><p>Phonebook has info for ${persons.length} people</p></div> <div><p>${Date()}</p><div/>`)
 })
