3572c2526ea82ea73d6af41ce1d59574a12af494
Sami Ollila <ollila.sami93@gmail.com>
2020-03-06 09:53:34 +0200

    Completed 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -10,7 +10,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce(countLikes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (!blogs) {
+    return {}
+  }
+  const hasMoreLikes = (acc, cur) => {
+    if (cur.likes > acc.likes) {
+      return cur
+    }
+    return acc
+  }
+  return blogs.reduce(hasMoreLikes, blogs[1])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
