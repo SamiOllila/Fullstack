b6d91ce6fab8a9c05ebce172ec465279371dab83
Sami Ollila <ollila.sami93@gmail.com>
2020-03-13 18:52:58 +0200

    Completed 4.17

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,19 +1,29 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const result = await Blog.find({})
+  const result = await Blog
+    .find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(result)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  if (!request.body.title || !request.body.url) {
-    response.sendStatus(400)
-    return
+  const body = request.body
+  if (!body.title || !body.url) {
+    return response.sendStatus(400)
   }
-  const blog = new Blog(request.body)
-
+  const defUser = await User.find({})
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    user: defUser[0]._id
+  })
   await blog.save()
+  defUser[0].blogs = defUser[0].blogs.concat(blog.id)
+  await defUser[0].save()
+
   response.status(201).send(blog)
 })
 
--- osa4/blogilista/controllers/users.js
+++ osa4/blogilista/controllers/users.js
@@ -3,7 +3,8 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const result = await User.find({})
+  const result = await User
+    .find({}).populate('blogs', { title: 1, author: 1, url: 1, id: 1 })
   response.json(result)
 })
 
