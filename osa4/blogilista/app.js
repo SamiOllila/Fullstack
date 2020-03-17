47eaf389c357bcfa0c47919a05061ef436b57f6e
Sami Ollila <ollila.sami93@gmail.com>
2020-03-17 13:44:19 +0200

    Completed 4.18

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -6,6 +6,7 @@ const logger = require('./utils/logger')
 const { MONGODB_URI, PORT } = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const mongoUrl = MONGODB_URI
 logger.info(`Connecting to MongoDB...`)
@@ -21,5 +22,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
