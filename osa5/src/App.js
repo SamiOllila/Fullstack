eda96e250494ecbc56e5d5853ddebadbc01e9434
Sami Ollila <ollila.sami93@gmail.com>
2020-03-26 15:28:19 +0200

    Unfinished 5.1

--- osa5/src/App.js
+++ osa5/src/App.js
@@ -2,10 +2,16 @@ import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import Login from './components/Login'
 import blogService from './services/blogs'
+import userService from './services/users'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [login, setLogin] = useState({ username: "", password: ""})
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [allUsers, setAllUsers] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -13,16 +19,63 @@ const App = () => {
     )  
   }, [])
 
-  const handleLogin = () => {
-    return null
+  useEffect(() => {
+    userService.getAll().then(users =>
+      setAllUsers( users )
+    )  
+  }, [])
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      const users = await userService.getAll()
+      setAllUsers(users)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const getUserBlogs = (username) => {
+    const targetUser = allUsers.filter(user => user.username === username)
+    const userBlogs = blogs.filter(blog => blog.user === targetUser.id)
+    return userBlogs
+  }
+
+  if (user === null){
+    return (
+      <div>
+        {<Login username={username} password={password} handleUsernameChange={handleUsernameChange} handlePasswordChange={handlePasswordChange} handleLogin={handleLogin} />}
+      </div>
+    )
   }
 
   return (
     <div>
-      {<Login login={login} handleLogin={handleLogin} />}
+      <p>Logged in: {user.name}</p>
+      <p>blog id: {blogs[0].id}</p>
+      <p>blog id: {blogs[1].id}</p>
 
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {getUserBlogs(user.username).map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
--- osa5/src/components/Login.js
+++ osa5/src/components/Login.js
@@ -1,18 +1,18 @@
 import React from 'react'
-const Login = ({ login, handleLogin }) => (
+const Login = ({ username, password, handleUsernameChange, handlePasswordChange, handleLogin }) => (
   <div>
     <h1>Log in to application</h1>
-    <form>
+    <form onSubmit={handleLogin}>
       <div>
         <label>
           Username: 
-          <input type="text" value={login.username} onChange={handleLogin} />
+          <input type="text" value={username} onChange={handleUsernameChange} />
         </label>
       </div>
       <div>
         <label>
           Password: 
-          <input type="text" value={login.password} onChange={handleLogin} />
+          <input type="text" value={password} onChange={handlePasswordChange} />
         </label>
       </div>
       <input type="submit" value="Login" />
